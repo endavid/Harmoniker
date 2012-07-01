26027116dfe5d6cccd7be8a6cc92941ee704f8b3
David Gavilan <david.gavilan.ruiz@gmail.com>
2012-07-01 17:26:55 +0900

    Initial Commit

---
+++ Harmoniker/MyTextureMap.h
@@ -0,0 +1,15 @@
+//
+//  MyTextureMap.h
+//  Harmoniker
+//
+//  Created by David Gavilan on 7/1/12.
+//  Copyright 2012 vid Games. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface MyTextureMap : NSPersistentDocument {
+
+}
+
+@end
---
+++ Harmoniker/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Harmoniker
+//
+//  Created by David Gavilan on 7/1/12.
+//  Copyright 2012 vid Games. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
